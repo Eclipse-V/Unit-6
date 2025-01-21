1d4068d61b640a4e7e1a0af78e8ac630afbcb85f
Eclipse-V <christinevanson77@gmail.com>
2025-01-21 14:13:47 -0500

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main{
+    public static void main(String[] args){
+        //CardGameGUI gui = new CardGameGUI(new ElevensBoard());
+        //gui.displayGame();
+       // Card c1 = new Card("K", "Spades", 5);
+        System.out.println("hi");
+    }
+}
