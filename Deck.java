d75405fbefc8d284563fd0299067a6e089c9adba
Eclipse-V <christinevanson77@gmail.com>
2025-01-26 17:39:20 -0500

    Update Deck.java

--- Deck.java
+++ Deck.java
@@ -2,5 +2,25 @@ class Deck{
     private Card[] cards;
     private int size;
 
+    public Deck(String[] ranks, String[] suits, int[] pointValues){
+        cards = ranks.length * suits.lengths;
+        
+
+    }
+
+    public boolean isEmpty(){
+        if (size == 0){
+            return true;
+        }
+        return false;
+    }
+
+    public int size(){
+        return size;
+    }
+
+    public Card deal(){
+
+    }
 
 }
